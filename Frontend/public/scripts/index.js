6a2563d13ed1c59e449cd164b0a6c0999cf32b60
Abdullah Wafy <red.sulphur786@gmail.com>
2020-04-21 16:04:31 -0700

    modal

--- Frontend/public/scripts/index.js
+++ Frontend/public/scripts/index.js
@@ -1,15 +1,43 @@
 const socket = io.connect("http://localhost:8080");
 window.addEventListener("DOMContentLoaded", event => {
+    let chatContainer = document.querySelector(".chat-container");
     // Get the modal
     let modal = document.getElementById("modal");
 
     // Get the button that opens the modal
     let button = document.getElementById("button");
 
+    let channelDropdown = document.getElementById("channelDropdown");
+    let channels = document.querySelector(".channel");
+
+    
+    // let dm = document.querySelector(".directMessage");
+    let dmDropdown = document.getElementById("dmDropdown");
+    let dms = document.querySelector(".dms");
 
     button.addEventListener("click", event => {
         modal.style.display = "block";
     });
+
+    chatContainer.addEventListener("click", e => {
+        if (modal.style.display === "block") {
+            modal.style.display = "none";
+        }
+    });
+
+    channelDropdown.addEventListener("click", e => {
+        
+        channels.classList.toggle("channel");
+    });
+
+    dmDropdown.addEventListener("click", e => {
+        
+        dms.classList.toggle("dms");
+    });
+    
+    const profile = document.getElementById("profile");
+    
+
     const input = document.getElementById("messages")
     const broadcast = document.querySelector(".broadcast")
     const messageDisplay = document.querySelector(".texts")
@@ -31,3 +59,6 @@ window.addEventListener("DOMContentLoaded", event => {
         broadcast.innerHTML = `<div><em>${data} is typing a message...</em</div>`;
     });
 })
+
+
+
