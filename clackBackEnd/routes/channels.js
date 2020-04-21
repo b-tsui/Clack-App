31b9fb1316de26ed15f6da0be21c6bb768096e5a
Brandon Tsui <itstsui@gmail.com>
2020-04-21 13:22:37 -0400

    completed channels route

--- clackBackEnd/routes/channels.js
+++ clackBackEnd/routes/channels.js
@@ -4,10 +4,17 @@ const { check } = require("express-validator");
 const { getUserToken, requireAuth } = require("../auth");
 const db = require("../db/models");
 
-const { Channel, User } = db;
+const { Channel, User, Message, ChannelUser } = db;
 
 const router = express.Router();
 const { asyncHandler, handleValidationErrors } = require("../utils");
+
+const userPermissionError = () => {
+    const err = Error('You do not have permission to do this');
+    err.title = "User alert.";
+    err.status = 404;
+    return err;
+};
 
 router.get('/:channelId/members', asyncHandler(async (req, res) => {
     const channelId = parseInt(req.params.channelId, 10);
@@ -18,4 +25,56 @@ router.get('/:channelId/members', asyncHandler(async (req, res) => {
     const [{ Users }] = members;
     res.json({ Users });
 }));
+
+router.get('/:channelId/messages', asyncHandler(async (req, res) => {
+    const channelId = parseInt(req.params.channelId, 10);
+    const channelMessages = await Channel.findAll({
+        include: [{ model: Message, attributes: ["message", "userId", "createdAt"] }],
+        where: { id: channelId }
+    });
+    const [{ Messages }] = channelMessages;
+    res.json({ Messages });
+}));
+
+
+router.post('/', requireAuth, asyncHandler(async (req, res) => {
+    const { userId, name, isDM, } = req.body;
+    const channel = await Channel.create({ userId, name, isDM });
+    const channelUsers = await ChannelUser.create({ userId, channelId: channel.dataValues.id })
+    res.status(204).json({ channel });
+}));
+
+router.put('/:channelId', asyncHandler(async (req, res, next) => {
+    const { userId, name } = req.body; //userId should be passed into the request
+    const channelId = parseInt(req.params.channelId, 10);
+    const channel = await Channel.findByPk(channelId);
+    if (channel && (Number(userId) === channel.dataValues.userId)) {
+        await channel.update({ name });
+        res.json({ channel });
+    } else {
+        next(userPermissionError());
+    }
+}));
+
+router.delete('/:channelId', asyncHandler(async (req, res, next) => {
+    const { userId } = req.body; //userId should be passed into the request
+    const channelId = parseInt(req.params.channelId, 10);
+    const channel = await Channel.findByPk(channelId);
+    const channelUsers = await ChannelUser.findAll({ where: { channelId } });
+    const messages = await Message.findAll({ where: { channelId } });
+    if (channel && (Number(userId) === channel.dataValues.userId)) {
+        for (let message of messages) {
+            await message.destroy();
+        }
+
+        for (let channelUser of channelUsers) {
+            await channelUser.destroy();
+        }
+        await channel.destroy();
+        res.status(204).end();
+    } else {
+        next(userPermissionError());
+    }
+}));
+
 module.exports = router;
