f0eb8624204715c042780b66faa9c247439d5cbd
Brandon Tsui <itstsui@gmail.com>
2020-04-21 18:54:18 -0400

    Linked up live chat on main page to backend

--- Frontend/public/scripts/index.js
+++ Frontend/public/scripts/index.js
@@ -1,5 +1,49 @@
 const socket = io.connect("http://localhost:8080");
-window.addEventListener("DOMContentLoaded", event => {
+document.addEventListener("DOMContentLoaded", async (event) => {
+    //grabbing userid from local storage
+    const userId = localStorage.getItem("CLACK_CURRENT_USER_ID");
+    const name = localStorage.getItem("CLACK_CURRENT_USER_FULLNAME");
+    try {
+        const user = await fetch(`http://localhost:8000/users/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('CLACK_ACCESS_TOKEN')}`
+            }
+        })
+
+        if (user.status === 401) {
+            window.location.href = '/log-in';
+            return
+        }
+        //grabbing messages for main page
+        const allMessages = await fetch(`http://localhost:8000/channels/1/messages`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('CLACK_ACCESS_TOKEN')}`
+            }
+        })
+        const { Messages } = await allMessages.json()
+        console.log(Messages);
+        const messagesContainer = document.querySelector(".texts");
+        const messagesHTML = Messages.map(
+            ({ message, User: { fullName }, createdAt }) => `
+			<div class="message">
+				<span class="message-author">
+					<strong>${fullName}</strong> : 
+				</span>
+				<span class="message-body">
+				<span class="message-text"> ${message}</span>
+                </span>
+                <span class="timestamp">
+                     (${new Date(createdAt).getHours()}:${new Date(createdAt).getMinutes()}:${new Date(createdAt).getSeconds()})
+                </span>
+			</div>
+			`
+        );
+        messagesContainer.innerHTML += messagesHTML.join("");
+    }
+    catch (e) {
+        console.error(e);
+    }
+
     // Get the modal
     let modal = document.getElementById("modal");
 
@@ -13,9 +57,25 @@ window.addEventListener("DOMContentLoaded", event => {
     const input = document.getElementById("messages")
     const broadcast = document.querySelector(".broadcast")
     const messageDisplay = document.querySelector(".texts")
-    input.addEventListener('keypress', function (e) {
+    input.addEventListener('keypress', async (e) => {
         if (e.key === 'Enter') {
-            socket.emit("chat", { message: input.value, sender: "test" });
+            try {
+                const message = await fetch(`http://localhost:8000/channels/1/messages`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem('CLACK_ACCESS_TOKEN')}`
+                    },
+                    body: JSON.stringify({
+                        userId,
+                        message: input.value,
+                    })
+                })
+            }
+            catch (e) {
+                console.error(e);
+            }
+            socket.emit("chat", { message: input.value, sender: name });
             input.value = "";
         }
     });
@@ -24,7 +84,8 @@ window.addEventListener("DOMContentLoaded", event => {
     });
     socket.on("chat", data => {
         broadcast.innerHTML = "";
-        messageDisplay.innerHTML += `<div><strong>${data.sender}:</strong> ${data.message}</div>`;
+        let chatTimeStamp = `(${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()})`
+        messageDisplay.innerHTML += `<div><strong>${data.sender}</strong> : ${data.message}    ${chatTimeStamp}</div>`;
     });
 
     socket.on("typing", data => {
