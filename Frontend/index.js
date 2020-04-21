67bb80616c5667ec4380d57de3da1469d4bc6337
Brandon Tsui <itstsui@gmail.com>
2020-04-21 16:09:51 -0400

    initialized socket.io

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const path = require("path");
 const app = express();
+const socket = require('socket.io')
+
+const port = 8080;
+
+const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
+
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('Socket Connection Established!!!', socket.id);
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data);
+
+    });
+    socket.on('typing', data => {
+
+        socket.broadcast.emit('typing', data);
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.render('splash');
@@ -21,8 +42,3 @@ app.get('/main', (req, res) => {
     res.render('main');
 });
 
-const port = 8080;
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
--- Frontend/public/scripts/index.js
+++ Frontend/public/scripts/index.js
@@ -1,3 +1,4 @@
+const socket = io.connect("http://localhost:8080");
 window.addEventListener("DOMContentLoaded", event => {
     // Get the modal
     let modal = document.getElementById("modal");
@@ -5,7 +6,28 @@ window.addEventListener("DOMContentLoaded", event => {
     // Get the button that opens the modal
     let button = document.getElementById("button");
 
+
     button.addEventListener("click", event => {
         modal.style.display = "block";
+    });
+    const input = document.getElementById("messages")
+    const broadcast = document.querySelector(".broadcast")
+    const messageDisplay = document.querySelector(".texts")
+    input.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            socket.emit("chat", { message: input.value, sender: "test" });
+            input.value = "";
+        }
+    });
+    input.addEventListener("keypress", even => {
+        socket.emit("typing", "test");
+    });
+    socket.on("chat", data => {
+        broadcast.innerHTML = "";
+        messageDisplay.innerHTML += `<div><strong>${data.sender}:</strong> ${data.message}</div>`;
+    });
+
+    socket.on("typing", data => {
+        broadcast.innerHTML = `<div><em>${data} is typing a message...</em</div>`;
     });
 })
