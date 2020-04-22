5744f546952eb45ea5ec31794204b7d7078c6e62
Abdullah Wafy <red.sulphur786@gmail.com>
2020-04-22 14:58:22 -0700

    icon bar, includes files, profile card

--- Frontend/public/scripts/index.js
+++ Frontend/public/scripts/index.js
@@ -45,68 +45,6 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         console.error(e);
     }
 
-    // Get the modal
-    let chatContainer = document.querySelector(".chat-container");
-    let modal = document.getElementById("modal");
-
-    // Get the button that opens the modal
-    let modalButton = document.getElementById("modalButton");
-
-    let channelDropdown = document.getElementById("channelDropdown");
-    let channels = document.querySelector(".channel");
-
-
-    // let dm = document.querySelector(".directMessage");
-    let dmDropdown = document.getElementById("dmDropdown");
-    let dms = document.querySelector(".dms");
-
-    modalButton.addEventListener("click", event => {
-        modal.style.display = "block";
-        modal.removeAttribute("modal-transform");
-    });
-    
-    //update the position/style for the profile pop-up to be on the right side once click on the 'view profile'
-    const profile = document.getElementById("profile");
-    profile.addEventListener("click", event =>{
-        modal.setAttribute("id", "modal-transform");
-        //add edit button for the profile
-        const editButton = document.createElement("button");
-        editButton.setAttribute("id", "editButton")
-        const textEditButton = document.createTextNode("Edit profile");
-        editButton.appendChild(textEditButton);
-        modal.appendChild(editButton);
-        //add message button to chat with the user
-        const chatButton = document.createElement("button");
-        chatButton.setAttribute("id", "chatButton")
-        const textChatButton = document.createTextNode("Message");
-        chatButton.appendChild(textChatButton);
-        modal.appendChild(chatButton);
-        //styling img for the user to be bigger by adding an id and styling it on side-panel.css
-        const avatar = document.getElementById("avatar");
-        avatar.setAttribute("id", "avatar-transform")
-        //removing the profile button 
-        profile.setAttribute("id", "profile-transform");
-        
-    })
-
-    chatContainer.addEventListener("click", e => {
-        if (modal.style.display === "block") {
-            modal.style.display = "none";
-        }
-    });
-
-    channelDropdown.addEventListener("click", e => {
-
-        channels.classList.toggle("channel");
-    });
-
-    dmDropdown.addEventListener("click", e => {
-
-        dms.classList.toggle("dms");
-    });
-
-    
-
 
     const input = document.getElementById("messages")
     const broadcast = document.querySelector(".broadcast")
@@ -140,11 +78,15 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         broadcast.innerHTML = "";
         let chatTimeStamp = `(${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()})`
         messageDisplay.innerHTML += `<div><strong>${data.sender}</strong> : ${data.message}    ${chatTimeStamp}</div>`;
+        
     });
 
     socket.on("typing", data => {
         broadcast.innerHTML = `<div><em>${data} is typing a message...</em</div>`;
+        //messageDisplay.scrollTop = messageDisplay.scrollHeight;
     });
+    
+    
 })
 
 
