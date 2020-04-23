bd29785d24e0804392774a87a34259f0a736568e
Abdullah Wafy <red.sulphur786@gmail.com>
2020-04-23 15:35:45 -0700

    broadcasting div, modal close buttons, emoji toggle"

---
+++ public/scripts/icon-bar.js
@@ -0,0 +1,11 @@
+// var quill = new Quill('#editor-container', {
+//     modules: {
+//         toolbar: [
+//             [{ header: [1, 2, false] }],
+//             ['bold', 'italic', 'underline'],
+//             ['', 'code-block']
+//         ]
+//     },
+    
+//     theme: 'snow'  // or 'bubble'
+// });
