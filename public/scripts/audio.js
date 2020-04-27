5dd716d54849375c8512d865ef06c25c2e5a3ffd
Brandon Tsui <itstsui@gmail.com>
2020-04-27 01:13:39 -0400

    hid features that are not fully functional

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -1,5 +1,7 @@
 const input = document.getElementById("messages");
 
+
+
 input.addEventListener('keyup', function (e) {
     if (e.key === 'Enter') {
         document.getElementById('audio').play();
